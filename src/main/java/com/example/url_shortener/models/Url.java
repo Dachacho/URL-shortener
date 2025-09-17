b782aace0043e0be9638caaf57ae55553f116cc4
Dachacho <arevadzedachi123@gmail.com>
2025-09-17 23:24:15 +0400

    feat: add url model

---
+++ src/main/java/com/example/url_shortener/models/Url.java
@@ -0,0 +1,17 @@
+package com.example.url_shortener.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Url {
+    @Id
+    private String id;
+    private String originalUrl;
+}
