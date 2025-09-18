adb47f6305e7759604914e909402ec3ed09e22db
Dachacho <arevadzedachi123@gmail.com>
2025-09-18 23:44:22 +0400

    feat: changed Url entity

--- src/main/java/com/example/url_shortener/models/Url.java
+++ src/main/java/com/example/url_shortener/models/Url.java
@@ -1,10 +1,14 @@
 package com.example.url_shortener.models;
 
+import jakarta.annotation.Nullable;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.Instant;
 
 @Entity
 @Data
@@ -13,5 +17,14 @@ import lombok.NoArgsConstructor;
 public class Url {
     @Id
     private String id;
+    @Column(nullable = false)
     private String originalUrl;
+    @Column(nullable = false)
+    private Instant createdAt = Instant.now();
+
+    private Instant expiresAt;
+
+    private boolean disabled = false;
+
+    private long visitCount = 0;
 }
